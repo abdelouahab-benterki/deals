e3d7b153a3f7f3256d24832bf0704519e565eb51
Abdelouahab Benterki <abdelouahab.benterki@hotmail.com>
2023-05-23 20:56:01 +0100

    new commit

--- products.js
+++ products.js
@@ -96,10 +96,10 @@ let products = [
     subInfo: "lorem ipsum dollar site nhg",
   },
   {
-    img: "./images/1.jpeg",
-    productName: "product name",
-    price: "28",
-    subInfo: "lorem ipsum dollar site nhg",
+    img: "https://get.pxhere.com/photo/shoe-sport-run-pink-leisure-fitness-running-shoe-tennis-shoe-product-sporty-women-sneakers-footwear-dynamic-magenta-brooks-product-photo-running-sports-sport-shoe-outdoor-shoe-athletic-shoe-nike-free-walking-shoe-cross-training-shoe-highly-functional-keen-on-sport-shoe-market-958791.jpg",
+    productName: "shoe",
+    price: "200",
+    subInfo: "lorem ipsum dollar site nhg dd",
   },
 ];
 
